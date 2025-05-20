bbfed7d468aaba66c8a0efca63623b11aed6cc01
jacobgsell15 <jacobgsell15@mail.com>
2025-05-20 11:20:41 -0400

    Adding Basic Ordering

--- backend/order/admin.py
+++ backend/order/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Order
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('title','description','completed')
 
 # Register your models here.
+
+admin.site.register(Order, OrderAdmin)
