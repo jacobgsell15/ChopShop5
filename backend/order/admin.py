0bb2de2cffad5346ac3dc5d43572a8258e1389f5
jacobgsell15 <jacobgsell15@mail.com>
2025-05-21 11:38:29 -0400

    update menu

--- backend/order/admin.py
+++ backend/order/admin.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
 from .models import Order
+from .models import Item
 
 class OrderAdmin(admin.ModelAdmin):
-    list_display = ('location','table','guests','completed')
+    list_display = ('location','table','guests','itemlist','item','completed')
+
+class ItemAdmin(admin.ModelAdmin):
+    list_display = ('menu_item','price','guest')
 
 # Register your models here.
 
 admin.site.register(Order, OrderAdmin)
+admin.site.register(Item, ItemAdmin)
--- backend/order/models.py
+++ backend/order/models.py
@@ -2,10 +2,17 @@ from django.db import models
 
 # Create your models here.
 
+class Item(models.Model):
+    menu_item = models.CharField(max_length=122,default="")
+    price = models.IntegerField()
+    guest = models.IntegerField()
+
 class Order(models.Model):
     location = models.CharField(max_length=120, default="")
-    table = models.CharField(max_length=120, default="")
-    guests = models.CharField(max_length=120)
+    table = models.IntegerField()
+    guests = models.IntegerField()
+    itemlist = models.JSONField()
+    item = models.ForeignKey(Item, on_delete=models.CASCADE)
     completed = models.BooleanField(default=False)
 
     def _str_(self):
