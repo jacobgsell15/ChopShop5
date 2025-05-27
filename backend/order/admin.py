2353552f767fea62166e99b47679c9c3c2162808
jacob <jacobgsell15@gmail.com>
2025-05-27 11:02:28 -0400

    retry

--- backend/order/admin.py
+++ backend/order/admin.py
@@ -6,11 +6,11 @@ class OrderAdmin(admin.ModelAdmin):
     list_display = ('table','location','guests','get_items','completed')
     
     def get_items(self, obj):  # Define your custom function
-            items = obj.items.all()
-            return ", ".join([item.description for item in items])  # Format the related data
+        items = obj.items.all()
+        return ", ".join([item.description for item in items])  # Format the related data
 
-        get_items.short_description = "Items"  # Optional: Set a descriptive label
-        get_items.admin_order_field = 'table'
+    get_items.short_description = "Items"  # Optional: Set a descriptive label
+    get_items.admin_order_field = 'table'
 
 class ItemAdmin(admin.ModelAdmin):
     list_display = ('description','recipe','modification','price')
