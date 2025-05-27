fda559fe1188d00b3ede5113f7bf61b420532e4f
jacob <jacobgsell15@gmail.com>
2025-05-27 12:28:50 -0400

    Remove original Order

--- backend/order/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import Order
-from .models import Item
-
-class OrderSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Order
-        fields = ('table', 'location', 'guests', 'item', 'completed')
