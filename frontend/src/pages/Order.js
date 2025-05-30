8c53d4cb5bc8034f9277be097e3982e6ca298658
jacob <jacobgsell15@gmail.com>
2025-05-30 16:30:51 -0400

    ed

--- frontend/src/pages/Order.js
+++ frontend/src/pages/Order.js
@@ -498,7 +498,7 @@ function Order(){
                 </div>
             </div>
             <>
-            <EditOrder curr={currOrder} menu={menu} />
+            <EditOrder curr={currOrd} menu={menu} />
             </>
             <div style={LocationsHouseDiv}>
                 {locs.map((location) => (
