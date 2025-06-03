ff030983f16730b04af9f791475f62bff7db9518
jacob <jacobgsell15@gmail.com>
2025-06-03 00:46:12 -0400

    Up

--- frontend/src/pages/Order.js
+++ frontend/src/pages/Order.js
@@ -194,7 +194,7 @@ function EditOrder(props){
         console.log(item)
         console.log(uitem)
         axios
-            .patch(`/api/additems/${item.id}/`, uitem)
+            .put(`/api/additems/${item.id}/`, uitem)
             .then((res) => this.refreshList());
         return;
         }
