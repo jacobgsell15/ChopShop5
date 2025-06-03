1ce2e9ff6fe3194999f1020d636265d9acfa9505
jacob <jacobgsell15@gmail.com>
2025-06-03 00:44:16 -0400

    Up

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -48,7 +48,7 @@ MIDDLEWARE = [
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     #'django.middleware.csrf.CsrfViewMiddleware',
-    'myapp.middle.DisableCSRFMiddleware',
+    'backend.middle.DisableCSRFMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
